47851cd25df6789a94f803e12dc8e21eb901c48f
tatyana-cloud-coder <56255005+tatyana-cloud-coder@users.noreply.github.com>
2020-11-28 00:06:44 +0400

    +Ninject
    Ninject

--- WebPL/Global.asax.cs
+++ WebPL/Global.asax.cs
@@ -1,17 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Ninject;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using Ninject.Web.Common.WebHost;
+using Gym.BLL.Interfaces;
+using Gym.BLL;
+using Gym.DAL.Interfaces;
+using Gym.DAL;
 namespace WebPL
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : NinjectHttpApplication
     {
-        protected void Application_Start()
+        public override void Init()
         {
+            base.Init();
+        }
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            Registration(kernel);
+            return kernel;
+        }
+        private void Registration(IKernel kernel)
+        {
+            
+            kernel.Bind<IClientLogic>().To<ClientLogic>().InSingletonScope();
+            kernel.Bind<ICoachLogic>().To<CoachLogic>().InSingletonScope();
+            kernel.Bind<IHallLogic>().To<HallLogic>().InSingletonScope();
+            kernel.Bind<IGroupLogic>().To<GroupLogic>().InSingletonScope();
+            kernel.Bind<IKindOfSportLogic>().To<KindOfSportLogic>().InSingletonScope();
+            kernel.Bind<IUserLogic>().To<UserLogic>().InSingletonScope();
+
+            kernel.Bind<IClientDao>().To<ClientDaoDB>().InSingletonScope();
+            kernel.Bind<ICoachDao>().To<CoachDaoDB>().InSingletonScope();
+            kernel.Bind<IHallDao>().To<HallDaoDB>().InSingletonScope();
+            kernel.Bind<IGroupDao>().To<GroupDaoDB>().InSingletonScope();
+            kernel.Bind<IKindOfSportDao>().To<KindOfSportDaoDB>().InSingletonScope();
+            kernel.Bind<IUserDao>().To<UserDaoDB>().InSingletonScope();
+            
+        }
+        protected override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
